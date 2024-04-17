47f5c63a244b7e631e486c34c587755b8308635f
Jeganathan Swaminathan <mail2jegan@gmail.com>
2024-04-17 15:16:35 +0530

    Added Day3 lab exercises.

---
+++ Day3/wordpress/deploy.sh
@@ -0,0 +1,13 @@
+oc apply -f wordpress-cm.yml 
+oc apply -f wordpress-secrets.yml 
+oc apply -f mariadb-pv.yml 
+oc apply -f mariadb-pvc.yml 
+oc apply -f mariadb-deploy.yml 
+oc apply -f mariadb-svc.yml 
+
+oc apply -f wordpress-pv.yml 
+oc apply -f wordpress-pvc.yml 
+oc apply -f wordpress-deploy.yml 
+oc apply -f wordpress-svc.yml 
+oc apply -f wordpress-route.yml 
+
